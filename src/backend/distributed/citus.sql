10dfb147312d1994af03070a1a8ae581f8860398
Nils Dijk <nils@citusdata.com>
2018-12-06 16:07:08 +0100

    fix windows tests by not trying to setup ssl when postgres doesn't support it

--- src/backend/distributed/citus.sql
+++ src/backend/distributed/citus.sql
@@ -25,7 +25,15 @@ CREATE FUNCTION citus_setup_ssl()
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$citus_setup_ssl$$;
 
-SELECT citus_setup_ssl();
+DO LANGUAGE plpgsql
+$$
+BEGIN
+    -- setup ssl when postgres is OpenSSL-enabled
+    IF current_setting('ssl_ciphers') != 'none' THEN
+        PERFORM citus_setup_ssl();
+    END IF;
+END;
+$$;
 
 DROP FUNCTION citus_setup_ssl();
 
